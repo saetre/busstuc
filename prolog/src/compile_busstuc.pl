e15d7393c48a6718a7b495ae97edf40a566709eb
satre <satre@7e2fc849-b7f8-e011-ab93-005056930022>
2011-11-21 01:26:26 +0000

    Trying to compile instead of consulting compile_busstuc.pl

---
+++ prolog/src/compile_busstuc.pl
@@ -0,0 +1,45 @@
+%% FILE compile_busstuc.pl
+%% SYSTEM BUSSTUC
+%% CREATED TA-040615
+%% REVISED TA-110208  FARA
+
+% Compiles the BussTUC system for NTNU server.
+
+?-compile('busstuc.pl'),
+  save_program('busestuc'), %% TA-110208
+  halt.
+
+
+/*   COMPILE FILE OVERVIEW
+
+
+ create_busstuc  (Unix command)
+
+ 
+     compile_busstuc.pl
+
+        busstuc.pl
+ 
+           tucbuss.pl
+
+               tucbuses.pl
+
+                   declare.pl
+                   utility/...pl
+                                    (sicstuc.pl)
+                   tuc/....pl
+                   tuc/gram_n.pl
+                   tuc/gram_e.pl
+                      ?-compile_english.                  
+                      ?-compile_norsk.
+               main.pl
+
+               monobus.pl
+
+                   app/...pl
+                   database/...pl
+
+           busroute.pl
+ 
+*/
+   
