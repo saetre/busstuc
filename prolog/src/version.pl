8ff519a65cfc82248dbed72279f252c165106f9b
satre <satre@7e2fc849-b7f8-e011-ab93-005056930022>
2012-02-06 10:21:25 +0000

    New routes again...

--- prolog/src/version.pl
+++ prolog/src/version.pl
@@ -1,8 +1,7 @@
 %% FILE version.pl
 %% SYSTEM TUC
 %% CREATED TA-970913
-/* REVISED : RS- */  version_date('AtB-H  Date 120203').
-
+/* REVISED : RS- */  version_date('AtB-H  Date 120206'). %% Hvor er Tore, og åpen sluppen bru
 
 
 
