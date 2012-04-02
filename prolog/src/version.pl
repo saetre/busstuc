efa09452d26051c114cac8668ab3dc621be8c0f0
satre <satre@7e2fc849-b7f8-e011-ab93-005056930022>
2012-04-02 09:48:15 +0000

    Easter Routes to May 16th, but "to lade" is NOT working!

--- prolog/src/version.pl
+++ prolog/src/version.pl
@@ -1,6 +1,6 @@
 %% FILE version.pl
 %% SYSTEM TUC
 %% CREATED TA-970913
-/* REVISED : RS- */  version_date('AtB-I  Date 120316'). %% Nye ruter...
+/* REVISED : RS- */  version_date('AtB-I  Date 120402'). %% Påske-ruter...
 
 
