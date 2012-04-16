c5bac72106282a6b47385a3820410273836531a5
satre <satre@7e2fc849-b7f8-e011-ab93-005056930022>
2012-04-16 21:33:03 +0000

    New May 17th routes

--- prolog/src/smstuc.pl
+++ prolog/src/smstuc.pl
@@ -1,11 +1,13 @@
 %% FILE smstuc.pl 
 %% SYSTEM TUC
 %% CREATED  TA-020125
-%% REVISED  TA-71119
+%% REVISED  RS-120416
 
 %% Makefile for SMSTUC   server, Whole
 
 ?-prolog_flag(unknown,_,fail).
+
+?-nodebug.      %% RS-120416, To speed up things
 
 ?- %%[sicstus4compatibility],   %% Only used in drucke_baum
 
--- prolog/src/version.pl
+++ prolog/src/version.pl
@@ -1,6 +1,6 @@
 %% FILE version.pl
 %% SYSTEM TUC
 %% CREATED TA-970913
-/* REVISED : RS- */  version_date('AtB-I  Date 120402'). %% Påske-ruter...
+/* REVISED : RS- */  version_date('AtB-I  Date 120416'). %% 17. mai-ruter, og småoppdateringer...
 
 
