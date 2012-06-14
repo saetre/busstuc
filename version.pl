0007c5402d0146ac4175b5dd105073451ef82229
satre <satre@7e2fc849-b7f8-e011-ab93-005056930022>
2012-06-14 10:03:46 +0000

    Sommerruter 2012

--- version.pl
+++ version.pl
@@ -1,6 +1,8 @@
 %% FILE version.pl
 %% SYSTEM TUC
 %% CREATED TA-970913
-/* REVISED : RS- */  version_date('AtB-I  Date 120423'). %% Fjernet buss 96 pga. bug i bus-plan
+/* REVISED : RS- */  version_date('AtB-I  Date 120614'). 
+
+%% Buss 96 er Fjernet i forrige set (til 24/6-12) pga. bug i bus-planleggingen
 
 
