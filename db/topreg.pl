5b69f39f64410aa278ce3ba8bc02cb913fff8e26
wollamo <wollamo@7e2fc849-b7f8-e011-ab93-005056930022>
2012-11-21 14:30:12 +0000

    renamed routes.pl to routePeriod.pl topreg includes routePeriod

--- db/topreg.pl
+++ db/topreg.pl
@@ -8,6 +8,7 @@
 %% Ny linje
 
 %% Contains the important information about route modules
+:-ensure_loaded(routes).
 
 :-volatile  thisdate_period_module/3 .       %% RS-120915
 :-dynamic   thisdate_period_module/3 .
@@ -62,6 +63,7 @@ default_message(tt,summer,['AtB summer routes are from Jun. 27 to  Aug. 21. 2012
 
 %% ACTUAL ROUTE PERIODS  SORTED ON --- END DATE ----
 
+
 % Xmas routes before winter routes
 % Easter routes before winter routes
 
@@ -72,30 +74,7 @@ default_message(tt,summer,['AtB summer routes are from Jun. 27 to  Aug. 21. 2012
 
 
 
-%%%% ACTUAL  ROUTES              START-DATE           END-DATE (sorted!)
 
-
-
-%% route_period( tt, r1618_110517, date(2011,05,17),    date(2011,05,17) ).
-%% route_period( tt, r1611_100823, date(2010,08,23),    date(2011,06,26) ).
-route_period( tmn, r1630_101201, date(2011,12,01),    date(2011,12,31) ).
-%route_period( tt, r1609_120120, date(2012,01,20),    date(2012,12,31) ).
-%route_period( tt, r1601_120201, date(2012,02,01),    date(2012,12,31) ).
-route_period( tt, r1602_120401, date(2012,04,01),    date(2012,04,08) ).
-%route_period( tt, r1603_120409, date(2012,04,09),    date(2012,05,12) ). %% Overwrote 120415
-route_period( tt, r1602_120517, date(2012,05,17),    date(2012,05,17) ).
-%route_period( tt, r1601_120415, date(2012,04,15),    date(2012,06,24) ).
-%route_period( tt, r1602_120425, date(2012,04,25),    date(2012,06,24) ).       %Mangler alle linjer over 79
-route_period( tt, r1604_120625, date(2012,06,25),    date(2012,08,12) ).
-%route_period( tt, r1611_110627, date(2012,06,27),   date(2012,08,14) ).
-
-%route_period( tt, r1602_120813, date(2012,08,13),    date(2012,09,12) ).
-route_period( tt, r1612_121001, date(2012,10,01),    date(2012,12,23) ).        %% New nightbusses etc. ver5 Høst 2012
-
-route_period( tt, r1601_120101, date(2012,01,01),    date(2012,12,31) ).
-route_period( gb, r1630_111201, date(2011,12,01),    date(2012,12,31) ).
-
-route_period( tt, r1612_121024, date(2012,10,24),    date(2013,03,24) ).        %% ny veiløsning på Ranheim. ver6 Høst 2012
 
 
 period_message( r1602_120517,
