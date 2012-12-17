db6b9f8fa136c689b6404a8d640c5e384b7716f1
runaa <runaa@7e2fc849-b7f8-e011-ab93-005056930022>
2012-12-17 13:23:42 +0000

    juleruter

--- compile/extract_cut.sh
+++ compile/extract_cut.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd /home/busstuc/busstuc
-echo "compile(busstuc),create_regcut($1)." | sicstus
+echo "compile(busstuc),create_regcut($1),halt." | sicstus
+
+#compile busstuc
+echo "compile(busstuc),save_program(busestuc4),halt." | sicstus
