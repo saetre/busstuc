5c6c0fbae15160c61f2b58b47b73ea41d8915d22
runaa <runaa@7e2fc849-b7f8-e011-ab93-005056930022>
2014-08-17 13:53:10 +0000

    Høstruter 2014

--- compile/extract_cut.sh
+++ compile/extract_cut.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/busstuc/busstuc
-echo "Make Regcut.pl"
+echo "/home/webdomener/www.idi.ntnu.no/htdocs/emner/itgk/sistenytt ... Make Regcut.pl"
 #echo "compile(busstuc),create_regcut($1),halt." | sicstus
 echo " use_module( 'compileroute.pl', [ compile_route_set/1 ] ), compile_route_set($1),
        use_module( 'utility/extracut.pl', [ create_regcut/1 ] ), create_regcut($1), halt." | /home/busstuc/busstuc/busestuc4.sav
